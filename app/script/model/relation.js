26f807a7d8e143aac6b6e7320e682e552592220e
Alfred Birketvedt <alfred_birk@hotmail.com>
2017-09-26 21:26:08 +0200

    Make arrow position and angle general for all positions and angles. Calculate text position too

--- app/script/model/relation.js
+++ app/script/model/relation.js
@@ -36,6 +36,7 @@ var Relation = (function () {
             y: coord["text"][type].y,
             text: title,
             fontSize: 14,
+            align: "center",
             fontFamily: 'Calibri',
             fill: 'black'
         });
@@ -47,9 +48,107 @@ var Relation = (function () {
         }
         Stage.layer.add(text);
     };
+
+    function getRectangleNodes(left, top, width, height) {
+            return [ [left + width / 2, top], [left + width, top + height / 2], [left + width / 2, top + height], [left, top + height / 2] ];
+        }
+
+    function squareDistance(x1, y1, x2, y2) {
+        return ( Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2) )
+    }
+
+    function getTextPosition(x1, y1, x2, y2, distanceFromObject, rightPerpendicular) {
+        const perpendicularDistanceFromLine = 20;
+
+        a = Math.sqrt(distanceFromObject**2 / (2*((x2-x1)*2 + (y2-y1)**2)))
+        var x = x1 + (x2-x1) * a
+        var y = y1 + (y2-y1) * a
+        console.log(a);
+
+        a2 = Math.sqrt(perpendicularDistanceFromLine**2 / (2*((x2-x1)*2 + (y2-y1)**2)))
+
+        if (rightPerpendicular) {
+          x += (y2-y1) * a2
+          y += (x1-x2) * a2
+        }
+        else {
+          x += (y1-y2) * a2
+          y += (x2-x1) * a2
+        }
+
+        console.log("a2:", a2);
+
+        return [x, y]
+
+    }
+
     Relation.prototype.computeSide = function (fromObject, toObject) {
-        //TODO: finish other sides
-        //right bottom
+
+        var fromPos = fromObject.data.position;
+        var toPos = toObject.data.position;
+
+        fromNodes = getRectangleNodes(fromPos.left, fromPos.top, fromPos.width, fromPos.height);
+        toNodes = getRectangleNodes(toPos.left, toPos.top, toPos.width, toPos.height);
+
+        var minDistance = 999999999
+        var minFrom;
+        var minTo;
+
+        for(var i = 0; i < fromNodes.length; i++) {
+            for(var j = 0; j < toNodes.length; j++) {
+                var d = squareDistance(fromNodes[i][0], fromNodes[i][1], toNodes[j][0], toNodes[j][1]);
+                if (d < minDistance) {
+                    minDistance = d
+                    minFrom = fromNodes[i]
+                    minTo = toNodes[j]
+                }
+            }
+        }
+
+        console.log("fromNodes: ", fromNodes);
+        console.log("toNodes: ", toNodes);
+        console.log("minFrom: ", minFrom);
+        console.log("minTo: ", minTo);
+
+
+        //todo: make more general
+        var rightPerpendicular = true;
+        if (minTo[1] < minFrom[1]) {
+          rightPerpendicular = false
+        }
+
+        var toDist = 100;
+        if (minTo[0] < minFrom[1]) {
+          toDist = 20
+        }
+
+        textFrom = getTextPosition(minFrom[0], minFrom[1], minTo[0], minTo[1], 30, rightPerpendicular)
+        textTo = getTextPosition(minTo[0], minTo[1], minFrom[0], minFrom[1], toDist, !rightPerpendicular)
+        console.log("textFrom", textFrom);
+
+        return {
+            line: {
+                from: {
+                    x: minFrom[0],
+                    y: minFrom[1]
+                },
+                to: {
+                    x: minTo[0] - minFrom[0],
+                    y: minTo[1] - minFrom[1]
+                }
+            },
+            text: {
+                from: {
+                    x: textFrom[0],
+                    y: textFrom[1],
+                },
+                to: {
+                    x: textTo[0],
+                    y: textTo[1]
+                }
+            }
+        };
+
         if (fromObject.data.position.left + fromObject.data.position.width <= toObject.data.position.left) {
             return {
                 line: {
